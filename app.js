0451de3dcd190fa538f5bd17937ddc1a62f74961
david <hdbv95@gmail.com>
2019-10-28 12:43:47 -0500

    get

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var morgan=require('morgan');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 var pruebaRutas=require('./public/js/Routes/routesWex');
+var credencialesWex=require('./public/js/Conexion/credencialesWex');
 var app = express();
 const server = require('http').createServer(app);
 var appEnv = cfenv.getAppEnv();
@@ -52,6 +53,27 @@ app.post('/webhook', (req, res) => {
 	  // Returns a '404 Not Found' if event is not from a page subscription
 	  res.sendStatus(404);
 	}  
+  });
+  // Adds support for GET requests to our webhook
+app.get('/webhook', (req, res) => {
+	// Your verify token. Should be a random string.
+	let VERIFY_TOKEN = credencialesWex.facebook.token;
+	// Parse the query params
+	let mode = req.query['hub.mode'];
+	let token = req.query['hub.verify_token'];
+	let challenge = req.query['hub.challenge'];	  
+	// Checks if a token and mode is in the query string of the request
+	if (mode && token) {	
+	  // Checks the mode and token sent is correct
+	  if (mode === 'subscribe' && token === VERIFY_TOKEN) {		
+		// Responds with the challenge token from the request
+		console.log('WEBHOOK_VERIFIED');
+		res.status(200).send(challenge);	  
+	  } else {
+		// Responds with '403 Forbidden' if verify tokens do not match
+		res.sendStatus(403);      
+	  }
+	}
   });
 ///
 
